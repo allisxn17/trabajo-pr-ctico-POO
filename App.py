285b0eb2563b262c235b18250f5078ac70f24871
zebax000 <sebasgamer8538@gmail.com>
2025-11-03 21:27:56 -0500

    Update App.py
    asegurar la carpeta

--- App.py
+++ App.py
@@ -1,12 +1,14 @@
+import sys, os
 import tkinter as tk
-from gui.main_gui import JuegoGUI
+# Asegurar que la carpeta raíz esté en el path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
+from gui.main_gui import JuegoGUI
 
 def main():
     root = tk.Tk()
     gui = JuegoGUI(root)
     root.mainloop()
-
 
 if __name__ == "__main__":
     main()
